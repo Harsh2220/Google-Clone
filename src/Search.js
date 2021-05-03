d3ed60968fbc12f8c878262064b0c5e20cda486b
Harsh2220 <harshgajjar145@gmail.com>
2021-05-03 14:51:57 +0530

    commit

---
+++ src/Search.js
@@ -0,0 +1,32 @@
+import { React, useState } from 'react';
+import { useHistory } from 'react-router';
+import './Search.css';
+import { Button } from '@material-ui/core';
+import { Mic, SearchOutlined } from '@material-ui/icons';
+
+function Search() {
+
+    const [input, setInput] = useState("");
+    const history = useHistory();
+
+    const search = (e) => {
+        e.preventDefault();
+        history.push("/search");
+    }
+
+    return (
+        <form className="search">
+            <div className="search_input">
+                <SearchOutlined />
+                <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+                <Mic />
+            </div>
+            <div className="search_buttons">
+                <Button type="submit" variant="outlined" onClick={search}>Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
+            </div>
+        </form>
+    )
+}
+
+export default Search;
